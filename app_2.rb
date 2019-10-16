63c2fa72ee18e64d70507cc0ea5a35db44d0f1d5
R David N H <rdavidnh@gmail.com>
2019-10-16 10:30:42 +0300

    updated files

--- app_2.rb
+++ app_2.rb
@@ -14,15 +14,32 @@ print "> "
 
 hplayer = HumanPlayer.new(gets.chomp)
 
-#enemies = [Player.new("Josiane"), Player.new("José")]
+enemies = [Player.new("Josiane"), Player.new("José")]
 player1 = Player.new("Josiane")
 player2 = Player.new("José")
 
-while hplayer.life_points > 0 && (player1.life_points > 0 || player2.life_points > 0)
+
+#while hplayer.life_points > 0 && (player1.life_points > 0 || player2.life_points > 0)
+while hplayer.life_points > 0
+  
+  # number of enemies killed
+  n_enemies = 0
+  enemies.each do |enemy|
+    if enemy.life_points <= 0
+      n_enemies += 1
+    end
+  end
+  # break loop if no more enemies
+  break if enemies.length == n_enemies
+
+  # show players states
   puts "Voici l'état de chaque joueur : "
   hplayer.show_state
-  player1.show_state
-  player2.show_state
+  
+  # show enemies states
+  enemies.each do |enemy|
+    enemy.show_state
+  end
   
   puts "===================================="
   
@@ -32,27 +49,34 @@ while hplayer.life_points > 0 && (player1.life_points > 0 || player2.life_points
   puts "s - chercher à se soigner "
   puts "----------------------------------"
   puts "attaquer un joueur en vue :"
-  print "0 - "
-  player1.show_state
-  print "1 - " 
-  player2.show_state 
-  
+
+  # show enemies states  
+  enemies.each_with_index do |enemy, i|
+    print "#{i} -"
+    enemy.show_state
+  end
+
   action = gets.chomp
 
+  # define possible actions for the player
   if action == "a"
     hplayer.search_weapon
-  end
-  
-  if action == "s"
+  elsif action == "s"
     hplayer.search_health_pack
+  elsif action.match(/^(\d)+$/) # check if action is a number
+    hplayer.attacks(enemies[action.to_i])
   end
   
-  if action == "0"
-    hplayer.attacks(player1)
-  elsif action == "1"
-    hplayer.attacks(player2)
+  # enemies turn to attack the player
+  enemies.each_with_index do |enemy, i|
+    if enemy.life_points > 0
+      enemy.attacks(hplayer)
+    end
   end
   
+  print "Appuyez sur Entrer pour continuer"
+  gets.chomp
+
   puts "===================================="
   
   #break
